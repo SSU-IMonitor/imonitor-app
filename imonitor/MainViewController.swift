860842b89ca36245804c80ed92d9b7ac49f299e7
YeeunHeo <gjdpdms2005@gmail.com>
2020-07-31 17:29:49 +0900

    header auto layout complete

--- imonitor/MainViewController.swift
+++ imonitor/MainViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    
+    let data = ["apple", "oranges", "grapes"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,4 +23,30 @@ class MainViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension MainViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension MainViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20.0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
 }
